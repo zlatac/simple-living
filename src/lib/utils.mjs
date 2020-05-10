6e544f4add23c652e3314d6c8591b5c81817a1e6
zlatac <[idahosa1adaghe@hotmail.ca]>
2020-05-10 15:35:28 -0400

    match algorithim draft

--- src/lib/utils.mjs
+++ src/lib/utils.mjs
@@ -18,6 +18,56 @@ const defineCustomElements = (elementClass, definitionName) => {
 
 }
 
+const rankAnalysis = (myRank, theirRank) => {
+	if (Array.isArray(myRank) && Array.isArray(theirRank) 
+		&& myRank.every(i => typeof i === 'number') && theirRank.every(i => typeof i === 'number')) {
+		// When both top ranks are the same they are 100% compatible
+		if (expressMatch()) {
+			return 1
+		}
+
+		return compromiseAnalysis()
+	}else {
+		throw new Error('Not an array or Array should only have number values')
+	}
+
+	function expressMatch() {
+		return myRank[0] === theirRank[0]
+	}
+
+	// They outcome of compatibility is heavily dependent on myRank
+	// as what is important to one user can be not important the other and vice versa
+	function compromiseAnalysis() {
+		let totalSum = 0
+		let matchProbabilty = 0
+		for(let i=myRank.length; i > 0; i--) {
+			totalSum += i;
+		}
+		const mean = Math.round(totalSum/myRank.length)
+		//console.log(mean, totalSum)
+		const myRankWeightMap = new Map()
+		for(let i=0; i < myRank.length; i++) {
+			const rankWeight = (myRank.length - i)/totalSum
+			const isLastIndex = (myRank.length - 1) === i
+			// The last index in the array will have a weight of 0
+			if (!isLastIndex) {
+				myRankWeightMap.set(myRank[i], rankWeight)
+			} else {
+				myRankWeightMap.set(myRank[i], 0)
+			}
+			
+		}
+		//console.log(myRankWeightMap)
+		for(let i=0; i < mean; i++) {
+			matchProbabilty += myRankWeightMap.get(theirRank[i])
+		}
+		//console.log(matchProbabilty)
+
+		return Number(Number(matchProbabilty).toFixed(2))
+	}
+}
+
 export default {
 	defineCustomElements,
+	rankAnalysis
 }
