242c8e08d716c0b6789ce6afded23df10880dd0f
zlatac <idahosa1adaghe@hotmail.ca>
2020-05-24 13:29:38 -0400

    Add database table schema for str-register

---
+++ src/database/str_register.sql
@@ -0,0 +1,35 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+
+CREATE TABLE `str_register` (
+  `id` int(11) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `phone` varchar(100) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `city` varchar(200) NOT NULL,
+  `industry` varchar(100) NOT NULL,
+  `living_status` varchar(50) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `email_intro` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+ALTER TABLE `str_register`
+  ADD PRIMARY KEY (`id`);
+
+
+ALTER TABLE `str_register`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
