1643d6e6f8d59391c7cca437a1b4e00d59a2e6fb
Zlatac <idahosa1adaghe@hotmail.ca>
2020-05-24 11:35:06 -0400

    Eugenejardell features/str 16
    * App-Card-vr1
    
    * App-Card-vr2
    
    * STR-16. Define AppCard, adjust slot, adjust css and test
    
    Co-authored-by: eugenejardell <eugenejardell@gmail.com>

---
+++ src/components/AppCard.mjs
@@ -0,0 +1,10 @@
+export default class AppCard extends HTMLElement {
+	constructor() {
+        super() 
+        const template = document.querySelector('#app-card').innerHTML
+        const shadow = this.attachShadow({mode: 'open'})
+        shadow.innerHTML = template
+    }
+}
+
+const Card = customElements.define('app-card', AppCard)
