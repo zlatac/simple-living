19e86429b98af5b077977048aa9206a45504a78a
eugenejardell <eugenejardell@gmail.com>
2020-06-02 02:21:18 -0400

    STR17-vr3

--- src/components/AppButton.mjs
+++ src/components/AppButton.mjs
@@ -1,10 +1,28 @@
 export default class AppButton extends HTMLElement {
-	constructor() {
-        super() 
-        const template = document.querySelector('#app-button').innerHTML
+
+static get observedAttributes() {
+        return ['type']
+    }
+    
+    constructor() {
+        super()
+		const template = document.querySelector('#app-button').innerHTML
         const shadow = this.attachShadow({mode: 'open'})
         shadow.innerHTML = template
+        const elem = this.shadowRoot.querySelector('.change-type')
+        elem.classList.add(this.type)
+        console.log(this.type)
+        //dom method allows you to add a new element. New child element to parent element
+        //instence of the custom element - the class (shadowroot). 
     }
+    
+ get type(){
+    let type = this.getAttribute('type')
+        if(type) {
+            return type    
+        }
+ }
+ 
 }
 
-const Card = customElements.define('app-button', AppButton)
+const button = customElements.define('app-button', AppButton)
